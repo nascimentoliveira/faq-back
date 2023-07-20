7ce38776299d4b5457f322b8103c8eb0d4ed608d
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-07-20 11:59:55 -0300

    feat: create faq controller

--- src/faq/faq.controller.ts
+++ src/faq/faq.controller.ts
@@ -1,20 +1,54 @@
-import { Controller, Get, Put, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Put,
+  Post,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { Question } from '@prisma/client';
+import { FaqService } from './faq.service';
+import { QuestionWithSubquestions } from './model/question.model';
+import { QuestionDTO } from './dto/question.dto';
 
 @Controller('faq')
 export class FaqController {
-  
+  constructor(private readonly faqService: FaqService) { }
+
   @Get()
-  getFaq() {
-    return 'Endpoint GET /faq';
+  async getAll(): Promise<QuestionWithSubquestions[]> {
+    return this.faqService.getAll();
   }
-  
-  @Put()
-  updateFaq() {
-    return 'Endpoint PUT /faq';
+
+  @Get(':id')
+  async getById(@Param('id') id: string): Promise<QuestionWithSubquestions[]> {
+    return this.faqService.getById(parseInt(id));
   }
-  
-  @Delete()
-  deleteFaq() {
-    return 'Endpoint DELETE /faq';
+
+  @Post()
+  async createParent(@Body() data: QuestionDTO): Promise<Question> {
+    return this.faqService.createParent(data);
+  }
+
+  @Post(':id')
+  async createSubquestion(
+    @Param('id') id: string,
+    @Body() data: QuestionDTO,
+  ): Promise<Question> {
+    return this.faqService.createSubquestion(parseInt(id), data);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() data: QuestionDTO,
+  ): Promise<Question> {
+    return this.faqService.update(parseInt(id), data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<Question> {
+    return this.faqService.delete(parseInt(id));
   }
 }
