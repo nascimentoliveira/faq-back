4096405f4750c427d4dae4b8dfe0d13f61890c09
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-07-21 02:33:32 -0300

    feat: config env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as cors from 'cors';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(cors());
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
